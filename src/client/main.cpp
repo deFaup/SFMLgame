61b359d21c42c0a3ad5216b4e31f615952b31ef6
antoine delavoypierre <antoine.delavoypierre@ensea.fr>
2018-10-17 15:38:49 +0200

    main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -20,7 +20,19 @@ using namespace state;
 int main(int argc,char* argv[]) 
 {
 
-    cout << "Bienvenue chez antoine et gregoire !" << endl;
-
-    return 0;
+	cout << "Bienvenue chez antoine et gregoire !" << endl;
+	if(argc == 2 /*&& argv[1] == "test"*/){
+		cout << "Test de la classe Statistiques" << endl;
+		Statistics stats;
+		unsigned int vie,attaque,deplacement;
+		cout << "Entrer nombre de point de vie (valeur entre 0 et 2000)" << endl;
+		cin >> vie;
+		cout << "Entrer nombre de point d'attaque (valeur entre 0 et 2000)" << endl;
+		cin >> attaque;
+		cout << "Entrer nombre de point de deplacement (valeur entre 0 et 2000)" << endl;
+		cin >> deplacement;
+		stats.Set_statistiques(vie,attaque,deplacement);
+		cout << "\n" << "vie = "<< stats.Get_life_points() << "\n" << "attaque = " << stats.Get_attack_points() << "\n" << "deplacement = " << stats.Get_move_points() << endl;
+	}
+	return 0;
 }
