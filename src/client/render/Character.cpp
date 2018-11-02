f656ac1839a6d44223058cfde08f940666cfdcf4
gregoire de faup <gregoire.de-faup@ensea.fr>
2018-11-02 02:05:04 +0100

    swicth characters

--- src/client/render/Character.cpp
+++ src/client/render/Character.cpp
@@ -1,6 +1,5 @@
 /* Includes */
 #include "define.hpp"
-#include "state.h"
 #include "Character.h"
 #include "TileSet.h"
 #include <memory>
@@ -8,41 +7,57 @@
 #include <iostream>
 
 using namespace render;
+using namespace state;
 using namespace std;
-
-void Character::fill_characters_list(){
-	for(unsigned int i = 0; i < state.get_number_of_player(); i++){
-		for(unsigned int j = 0; j < state.players[i]->get_number_of_characters(); j++){
-			characters.push_back(state.players[i]->get_character(i));
-		}
-	}
-
-}
 
 Character::Character(const state::GameState& state) : state(state)
 {
-
+	/* instantiation of a unique work surface */
 	surface = std::make_unique<Surface>();
 
-	//const vector<shared_ptr<state::Characters>>& state_characters = state.get_characters();
-
+	/* get all existing characters */
 	fill_characters_list();
 
 	for (int i = 0; i < characters.size(); i++)
 	{
-		const string name = /*state_*/characters[i]->get_name();
-		if (name == "goku")
+		/* create a tile to find the sprite we want in our tileset */
+		Tile tile;
+
+		const CharactersID id = characters[i]->get_id();
+		switch (id)
 		{
-			tileset.push_back(std::make_shared<TileSet>("res/DBZ_gokusheet2.gif"));
-			sf::Image image = tileset[0]->getImage();
-			image.createMaskFromColor(image.getPixel(1, 1));
+			case default_value:
+				//life_point = 100;
+			break;
 
-			/* créer une surface */
-			surface->loadTexture(image);	// appel à load texture
-			Tile tile(134, 192, 80, 80);
-			surface->setSpriteTexture(tile);
+			case goku:
+				tileset.push_back(std::make_shared<TileSet>("res/DBZ_gokusheet2.gif"));
+				tile.setTile(134, 192, 80, 80);
+			break;
+
+			case vegeta:
+				//life_point = 100;
+			break;
+
+			case miyo:
+				//life_point = 100;
+			break;
 		}
+		sf::Image image = tileset[0]->getImage();
+		image.createMaskFromColor(image.getPixel(1, 1));
 
+		surface->loadTexture(image);
+		surface->setSpriteTexture(tile);
+		surface->setSpriteLocation(characters[i]->get_position());
 	}
+}
 
+void Character::fill_characters_list() {
+	for (unsigned int i = 0; i < state.get_number_of_player(); i++)
+	{
+		for (unsigned int j = 0; j < state.get_player(i)->get_number_of_characters(); j++)
+		{
+			characters.push_back(state.players[i]->get_character(i));
+		}
+	}
 }
