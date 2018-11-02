67604d2f39962082370c649b6ffebb3ef6c2f86d
gregoire de faup <gregoire.de-faup@ensea.fr>
2018-11-02 02:04:07 +0100

    setTile

--- src/client/render/Tile.cpp
+++ src/client/render/Tile.cpp
@@ -1,5 +1,13 @@
 #include "Tile.h"
 using namespace render;
 
-Tile::Tile() {}
+Tile::Tile(): x(0), y(0), width(0), height(0) {}
 Tile::Tile(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
+
+void Tile::setTile(int x, int y, int width, int height)
+{
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
