9f9d7e10523b8d1b9c6c966bf80f0931d7134f39
gregoire de faup <gregoire.de-faup@ensea.fr>
2018-11-07 11:25:25 +0100

    view border in update view fully working

--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -24,9 +24,9 @@ void Scene::draw()
 {
 	sf::RenderWindow renderWindow;
 	sf::View window_view;
-	sf::Vector2f position_in_map;
-
+	sf::Vector2f position_in_map[2]; //[1] is the position with mouse coordinates
 	init_window(window_view, renderWindow, position_in_map);
+	const sf::Vector2f init_position = position_in_map[0];
 
 	while (renderWindow.isOpen()) 
 	{
@@ -40,7 +40,7 @@ void Scene::draw()
 		}
 		renderWindow.clear();
 
-		update_view(window_view, renderWindow, position_in_map);
+		update_view(window_view, renderWindow, position_in_map, init_position);
 
 		background.setSurface(renderWindow); // first the background
 		characters.setSurface(renderWindow); // on top of the background the characters
@@ -49,7 +49,7 @@ void Scene::draw()
 }
 
 /* config the view position for the beginning */
-void Scene::init_window(sf::View& window_view, sf::RenderWindow& renderWindow, sf::Vector2f& position_in_map)
+void Scene::init_window(sf::View& window_view, sf::RenderWindow& renderWindow, sf::Vector2f (&position_in_map) [2])
 {
 	int width(0), height(0);
 	state::Map::get_screen_dimensions(width, height);
@@ -60,11 +60,12 @@ void Scene::init_window(sf::View& window_view, sf::RenderWindow& renderWindow, s
 //	window_view.setSize(3000, 2000);
 
 	// set the first position in the game: temporary
-	position_in_map.x = width/2 ; 	position_in_map.y = height/2;
-	sf::Mouse::setPosition((sf::Vector2i)position_in_map);
+	position_in_map[0].x = position_in_map[1].x = width/2 ;
+	position_in_map[0].y = position_in_map[0].y = height/2;
+	sf::Mouse::setPosition((sf::Vector2i)position_in_map[0]);
 
 	// set the view center so that the top left corner of the map is the top left corner of the window
-	window_view.setCenter(position_in_map);
+	window_view.setCenter(position_in_map[0]);
 
 	//position_in_map.x = 0; 	position_in_map.y = 0;
 
@@ -73,40 +74,43 @@ void Scene::init_window(sf::View& window_view, sf::RenderWindow& renderWindow, s
 }
 
 /* If we hit the border of the window then we move the view accordingly */
-void Scene::update_view(sf::View& window_view, sf::RenderWindow& renderWindow, sf::Vector2f& position_in_map)
+void Scene::update_view(sf::View& window_view, sf::RenderWindow& renderWindow, sf::Vector2f(&position_in_map)[2], sf::Vector2f init_position)
 {
 	unsigned int x = sf::Mouse::getPosition().x;
 	unsigned int y = sf::Mouse::getPosition().y;
+	position_in_map[1].x = position_in_map[0].x + x - init_position.x;
+	position_in_map[1].y = position_in_map[0].y + y - init_position.y;
 
 	//temporary
 	int map_size[2] = { 0 };
 	state.get_map().get_dimensions(map_size[0], map_size[1]);
 
 	if (x >= renderWindow.getPosition().x + renderWindow.getSize().x)
-		position_in_map.x += 10;
+		position_in_map[0].x += 10;
 
 	if (x <= renderWindow.getPosition().x)
-		position_in_map.x -= 10;
+		position_in_map[0].x -= 10;
 
 	if (y >= renderWindow.getPosition().y + renderWindow.getSize().y)
-		position_in_map.y += 10;
+		position_in_map[0].y += 10;
 
 	if (y <= renderWindow.getPosition().y)
-		position_in_map.y -= 10;
+		position_in_map[0].y -= 10;
 
-	if (position_in_map.x >= map_size[0])
-		position_in_map.x = map_size[0];
-	else if (position_in_map.x <= 0)
-		position_in_map.x = 0;
+	if (position_in_map[1].x >= map_size[0])
+		position_in_map[0].x -= 10;
+	else if (position_in_map[1].x <= 0)
+		position_in_map[0].x += 10;
 
-	if (position_in_map.y >= map_size[1])
-		position_in_map.y = map_size[1];
-	else if (position_in_map.y <= 0)
-		position_in_map.y = 0;
+	if (position_in_map[1].y >= map_size[1])
+		position_in_map[0].y -= 10;
+	else if (position_in_map[1].y <= 0)
+		position_in_map[0].y += 10;
 
 	renderWindow.setView(window_view);
-	window_view.setCenter(position_in_map);
+	window_view.setCenter(position_in_map[0]);
 /*	cout << x << " " << y << endl;
-	cout << position_in_map.x << " " << position_in_map.y << endl << endl;
+	cout << position_in_map[1].x << " " << position_in_map[1].y << endl;
+	cout << position_in_map[0].x << " " << position_in_map[0].y << endl << endl;
 */
 }
