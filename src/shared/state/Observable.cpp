aa0bf30e10ce3fc22fee6db25b32e13af77d69a3
antoine delavoypierre <antoine.delavoypierre@ensea.fr>
2018-11-07 16:38:48 +0100

    ajout code observer

---
+++ src/shared/state/Observable.cpp
@@ -0,0 +1,10 @@
+/*#include "define.hpp"
+#include "Observable.h"
+
+using namespace state;
+
+void Observable::registerObserver(Observer observer){
+	observers.push_back(observer);
+	return;
+}
+*/
---
+++ src/shared/state/Observer.cpp
@@ -0,0 +1,11 @@
+/*#include "define.hpp"
+#include "Observer.h"
+#include <iostream>
+
+using namespace state;
+
+void Observer::somethingHappen(){
+	std::cout << "quelque chose a changé" << std::endl;
+	return;
+}
+*/
