aa3615c277cedf42e3d720e88cb67f3c81440773
antoine delavoypierre <antoine.delavoypierre@ensea.fr>
2018-11-08 19:17:24 +0100

    mise a jour engine

--- src/shared/engine/Attack.cpp
+++ src/shared/engine/Attack.cpp
@@ -1,15 +1,24 @@
 #include "define.hpp"
 #include "Attack.h"
 #include <iostream>
+#include <memory>
 
 using namespace engine;
 using namespace std;
+using namespace state;
 
 int Attack::isLegit(state::GameState etat){
+	shared_ptr<Characters> character = etat.current_player->get_current_character();
+	Statistics& stats = character->get_statistics();
+	
+	if(character->get_attack_cost(attack_number) >= stats.get_attack_point()){
+		return(-1);
+	}
 	return(0);
 }
 
 void Attack::execute(state::GameState& etat){
+	
 	cout << "executing attack" << endl;
 	return;
 }
