62c7b14f15cc446c9c57df11a4fbbd8158cacabd
gregoire de faup <gregoire.de-faup@ensea.fr>
2018-11-20 15:06:57 +0100

    reset characters stats at each turn

--- src/shared/state/Statistics.cpp
+++ src/shared/state/Statistics.cpp
@@ -81,35 +81,26 @@ void state::Statistics::set_statistics(Statistics given_stats)
 	move_point = given_stats.move_point;
 }
 
-/*
-void state::Statistics::Set_statistics (int vie, int attaque, int deplacement){
-	if(0 <= vie && vie <= MAX_LIFE_POINT){
-		this->life_point = vie;
+void state::Statistics::reset_all_but_life(CharactersID id)
+{
+	switch (id)
+	{
+	case goku:
+		//life_point = 100;
+		attack_point = 10;
+		move_point = 100;
+		break;
+
+	case vegeta:
+		//life_point = 100;
+		attack_point = 10;
+		move_point = 100;
+		break;
+
+	case miyo:
+		//life_point = 100;
+		attack_point = 10;
+		move_point = 100;
+		break;
 	}
-	else if(vie > MAX_LIFE_POINT){
-		this->life_point = MAX_LIFE_POINT;
-	}
-	else{
-		this->life_point = 0;
-	}
-	if(0 <= attaque && attaque <= MAX_ATTACK_POINT){
-		this->attack_point = attaque;
-	}
-	else if(attaque > MAX_ATTACK_POINT){
-		this->attack_point = MAX_ATTACK_POINT;
-	}
-	else{
-		this->attack_point = 0;
-	}
-	if(0 <= deplacement && deplacement <= MAX_MOVE_POINT){
-		this->move_point = deplacement;
-	}
-	else if(deplacement > MAX_MOVE_POINT){
-		this->move_point = MAX_MOVE_POINT;
-	}
-	else{
-		this->move_point = 0;
-	}
-	return;
 }
-*/
