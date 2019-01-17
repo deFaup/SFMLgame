1d7c4fc4099eb0ac786bee5c3d22a6145eda7001
antoine <antoine.delavoypierre@ensea.fr>
2019-01-17 08:25:00 +0100

    modif dia CommandDB

--- src/server/server/CommandDB.cpp
+++ src/server/server/CommandDB.cpp
@@ -2,7 +2,39 @@
 
 using namespace server;
 using namespace std;
+/*
+forme du JSON :
 
+{
+	"commandes" :
+	[
+		{
+			"sfEventsID" : xxx,
+			"x" : xxx,
+			"y" : xxx
+		},
+		{
+			Et ainsi de suite ...
+		}
+	]
+}
+
+*/
 CommandDB::CommandDB()
 {
+}
+
+Json::Value& CommandDB::getCommand()
+{
+
+}
+
+void CommandDB::addCommand(Json::Value& commande)
+{
+
+}
+
+void CommandDB::deleteCommand(Json::Value& commande)
+{
+
 }
